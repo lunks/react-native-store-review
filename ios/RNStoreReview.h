3a5d877c26463d2a5e0f6fd0286e5ecf5845b7a8
Pedro Nascimento <pnascimento@gmail.com>
2019-09-11 13:29:58 -0300

    Fix import ordering to match Sentry

--- ios/RNStoreReview.h
+++ ios/RNStoreReview.h
@@ -1,7 +1,7 @@
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h> // This is used for versions of react >= 0.40
 #else
-#import <React/RCTBridgeModule.h>
+#import "RCTBridgeModule.h" // This is used for versions of react < 0.40
 #endif
 
 @interface RNStoreReview : NSObject <RCTBridgeModule>
